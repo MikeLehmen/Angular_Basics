1ca469c2bccdd8a8fec7c2246134711313a4df6c
MikeLehmen <mike.lehmen@gmail.com>
2019-10-01 15:57:47 -0500

    Changed messages and their display format

--- src/app/messages.service.ts
+++ src/app/messages.service.ts
@@ -13,16 +13,16 @@ export class MessagesService {
   initMessages(): Messages {
     let out : Messages = new Messages;
 
-    let newMessage = new Message(new User("Cthulu"), new Date("June 17, 1974 04:12:43"), "I'm still asleep...");
+    let newMessage = new Message(new User("Ralph"), new Date("June 17, 1974 04:12:43"), "Testing, testing....1, 2...");
     out.addMessage(newMessage);
 
-    newMessage = new Message(new User("Ralph"), new Date("March 15, 1802 14:47:21"), "I wonder what is going on out there.");
+    newMessage = new Message(new User("Ashley"), new Date("March 15, 1802 14:47:21"), "Hello world!");
     out.addMessage(newMessage);
 
     newMessage = new Message(new User("George"), new Date("November 2, 1992 18:13:58"), "Man, I'm beat.");
     out.addMessage(newMessage);
 
-    newMessage = new Message(new User("Ted"), new Date("January 29, 2003 22:43:03"), "Something something in Latin.");
+    newMessage = new Message(new User("Stacey"), new Date("January 29, 2003 22:43:03"), "What a long, strange trip it's been.");
     out.addMessage(newMessage);
 
     newMessage = new Message(new User("Homer"), new Date("October 31, 2018 17:15:37"), "All work and no play makes Homer go something something...");
